cd7c21ad26b3942b25ef9e1fe732c08c5a4e1ffd
Robo799 <s30936@pjwstk.edu.pl>
2025-03-31 22:39:16 +0200

    Refactored UserService.cs

---
+++ LegacyApp/UserService.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace LegacyApp
+{
+    public class UserService
+    {
+        public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
+        {
+            if (!IsValidUserData(firstName, lastName, email, dateOfBirth))
+                return false;
+
+            var client = new ClientRepository().GetById(clientId);
+            var user = CreateUser(firstName, lastName, email, dateOfBirth, client);
+
+            if (user.HasCreditLimit && user.CreditLimit < 500)
+                return false;
+
+            UserDataAccess.AddUser(user);
+            return true;
+        }
+
+        private bool IsValidUserData(string firstName, string lastName, string email, DateTime dob)
+        {
+            if (string.IsNullOrWhiteSpace(firstName) || string.IsNullOrWhiteSpace(lastName))
+                return false;
+
+            if (!email.Contains("@") || !email.Contains("."))
+                return false;
+
+            var now = DateTime.Now;
+            int age = now.Year - dob.Year;
+            if (now.Month < dob.Month || (now.Month == dob.Month && now.Day < dob.Day)) age--;
+
+            return age >= 21;
+        }
+
+        private User CreateUser(string firstName, string lastName, string email, DateTime dob, Client client)
+        {
+            var user = new User
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                EmailAddress = email,
+                DateOfBirth = dob,
+                Client = client
+            };
+
+            if (client.Type == "VeryImportantClient")
+            {
+                user.HasCreditLimit = false;
+            }
+            else
+            {
+                user.HasCreditLimit = true;
+                using (var creditService = new UserCreditService())
+                {
+                    int creditLimit = creditService.GetCreditLimit(lastName, dob);
+                    if (client.Type == "ImportantClient")
+                    {
+                        creditLimit *= 2;
+                    }
+                    user.CreditLimit = creditLimit;
+                }
+            }
+
+            return user;
+        }
+    }
+}
